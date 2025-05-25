a8b9627b93f084065babb7f2dadb18d3a2c79aa0
Luca1550 <luca-charles@hotmail.com>
2025-05-25 19:03:54 +0200

    [RES] Add_theme
    Resolved:
    issue was on lines 11 and 19.
    correct arg was "id" and not "id_theme"
    
    Co-Authored-By: ZekJulien <110969316+ZekJulien@users.noreply.github.com>

--- app/repositories/models/theme.py
+++ app/repositories/models/theme.py
@@ -8,7 +8,7 @@ class Theme(Base):
         id_theme (int | None): Unique identifier for the theme.
         name (str): Name of the theme.
     """
-    def __init__(self,id_theme:int | None,name : str):
+    def __init__(self,id:int | None,name : str):
         """
         Initializes a new Theme instance with a unique ID and a name.
         If an ID is provided, it will be used; otherwise, a new unique ID is generated.
@@ -16,5 +16,5 @@ class Theme(Base):
             id_theme (int | None): Unique identifier for the theme. If None, a new ID is generated.
             name (str): Name of the theme.
         """
-        super().__init__(id_theme)
+        super().__init__(id)
         self.name = name
