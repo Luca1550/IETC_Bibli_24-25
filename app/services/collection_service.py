690ad517b6466bf0783b8ea326929ef9530ea8c4
Luca1550 <luca-charles@hotmail.com>
2025-05-27 15:42:35 +0200

    [ADD] update collection
    adding update collection function

--- app/services/collection_service.py
+++ app/services/collection_service.py
@@ -18,7 +18,38 @@ class CollectionService :
         :return: None
         """
         try:
-            self.collection_repo.add_theme(name)
+            self.collection_repo.add_collection(name)
         except:
             print(f"Error adding collection: {name}")
             return f"Error adding collection: {name}"
+    
+    def get_by_collection(self,name):
+        """
+        Retrieves a collection by its name.
+        :param name: The name of the collection to retrieve.
+        :return: The collection object if found, None otherwise.
+        """
+        collection = self.collection_repo.get_by_collection(name)
+        if collection:
+            return f"Collection : {collection.name}"
+        else : 
+            return "Collection non trouvé"
+    
+    def get_by_id(self,id):
+        """
+        Retrieves a collection by its ID.
+        :param id: The ID of the collection to retrieve.
+        :return: The collection object if found, None otherwise.
+        """
+        collection = self.collection_repo.get_by_id(id)
+        if collection:
+            return f"Collection : {collection.name}"
+        else : 
+            return "Collection non trouvé"
+    
+    def get_all(self):
+        """
+        Retrieves all collections.
+        :return: A list of all collections.
+        """
+        return self.collection_repo.get_all()
