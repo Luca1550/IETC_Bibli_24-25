bedeb23856eead838cd4f4a9ab95ceac308e9b05
ZekJulien <110969316+ZekJulien@users.noreply.github.com>
2025-05-27 09:44:22 +0200

    ➕ [ADD] Person model
    Implemented the `Person` class, inheriting from `Base`, to represent individual users with essential attributes including personal details and contact information.
    
    - Added initialization method to define attributes such as `first_name`, `last_name`, `national_number`, `email`, and address details (`street`, `cp`, `city`).
    - Ensured that an `id` can either be explicitly provided or generated dynamically.
    - Structured the model for easy integration with repository management.

---
+++ app/repositories/models/person.py
@@ -0,0 +1,24 @@
+from repositories.models import Base
+
+class Person(Base):
+    def __init__(self, id : int | None, first_name : str, last_name : str, national_number : str, email : str, street : str, cp : str, city : str):
+        """
+        Initializes a new Person instance with the provided attributes.
+        Args:
+            id (int | None): Unique identifier for the person. If None, a new ID is generated.
+            first_name (str): First name of the person.
+            last_name (str): Last name of the person.
+            national_number (str): National identification number.
+            email (str): Email address of the person.
+            street (str): Street address of the person.
+            cp (str): Postal code of the person's address.
+            city (str): City of the person's address.
+        """
+        super().__init__(id)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.national_number = national_number
+        self.email = email
+        self.street = street
+        self.cp = cp
+        self.city = city
