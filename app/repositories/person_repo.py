65226b0926ed5160f9c80e133f1315294a0a01a4
Zek Julien <110969316+ZekJulien@users.noreply.github.com>
2025-05-30 00:25:53 +0200

    ➕ [ADD] Implement update person in PersonRepo and PersonService
    - Added `update_person()` method in `PersonRepo` to manage updates for `Person` objects in the JSON database.
    - Optimized `_check_person_value()` to centralize validations and reduce redundant checks.
    - ✨ Refactored 'person_niss_exist(self, niss : str)' to 'is_unique()' method in 'PersonRepo' to efficiently verify uniqueness of attributes.

--- app/repositories/person_repo.py
+++ app/repositories/person_repo.py
@@ -23,7 +23,7 @@ class PersonRepo:
         """
         JsonStorage.save_all(self.PATH_PERSON_JSON, self._person_json)
 
-    def add_person(self, person : Person):
+    def add_person(self, person : Person) -> bool:
         """
         Adds a Person object to the repository and saves it to the JSON file.
         arguments:
@@ -37,7 +37,7 @@ class PersonRepo:
             return True
         return False
     
-    def get_by_id(self, id : int):
+    def get_by_id(self, id : int) -> Person | bool:
         """
         Retrieves a Person object by its ID.
         arguments:
@@ -49,7 +49,21 @@ class PersonRepo:
             return next((p for p in self._person_json if p.id == id), None)
         return False
     
-    def delete_person(self, person : Person):
+    def update_person(self, person : Person) -> bool:
+        """
+        Updates an existing Person object in the repository and saves the changes to the JSON file.
+        arguments:
+        - person: Person object to be updated.
+        returns:
+        - True if the person was updated successfully, otherwise returns False.
+        """
+        if isinstance(person, Person):
+            self._person_json[self._person_json.index(person)] = person
+            self._save_all()
+            return True
+        return False
+
+    def delete_person(self, person : Person) -> bool:
         """
         Deletes a Person object from the repository and saves the changes to the JSON file.
         arguments:
@@ -63,12 +77,13 @@ class PersonRepo:
             return True
         return False
     
-    def person_niss_exist(self, niss : str):
+    def is_unique(self, attribute : str, value : object) -> bool:
         """
-        Checks if a Person with the given national number (NISS) exists in the repository.
+        Checks if a given attribute of a Person object is unique in the repository.
         arguments:
-        - niss: National number (NISS) to check for existence.
+        - attribute: The attribute to check for uniqueness (e.g., 'national_number', 'email').
+        - value: The value to check against the specified attribute.
         returns:
-        - True if a Person with the given NISS exists, False otherwise.
+        - True if the value is unique, False if it already exists in the repository.
         """
-        return any(person.national_number == niss for person in self._person_json)
+        return not any(getattr(person, attribute, None) == value for person in self._person_json)
