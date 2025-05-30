3c97ba1baaaaa39dfdf5f33dd2431ba4635c8e2f
CroiAlin <croitoru.alin.info@gmail.com>
2025-05-30 20:46:18 +0200

    [WORKERS] IMP - Updating branches

--- app/services/worker_service.py
+++ app/services/worker_service.py
@@ -1,6 +1,8 @@
 import re
-from repositories.models import Worker
+from repositories.models import Worker.
+from repositories.models import Person
 from repositories import WorkerRepo
+from repositories import PersonRepo
 
 class WorkerService:
     """
@@ -12,6 +14,32 @@ class WorkerService:
         """
         self._worker_repo: WorkerRepo = WorkerRepo()
 
+    def add_worker(self, person:Person) -> Worker | str:
+        """
+        Adds a new worker to the repository.
+        arguments:
+        - person: Person object containing worker details.
+        returns:
+        - Returns a Worker object if added successfully, otherwise returns an error message.
+        """
+        try:
+            if isinstance(person, Person):
+                new_worker = Worker(
+                    id=None,
+                    first_name=person.first_name,
+                    last_name=person.last_name,
+                    national_number=person.national_number,
+                    email=person.email,
+                    street=person.street,
+                    cp=person.cp,
+                    city=person.city
+                )
+                if self._worker_repo.add_worker(new_worker):
+                    return new_worker
+            else:
+                raise TypeError("Provided person is not a valid Person object.")
+        except Exception as e:
+            return f"🛑 Error [{e}]"
 
     def delete_worker(self, id: int) -> bool:
         """
@@ -43,4 +71,21 @@ class WorkerService:
         if isinstance(worker, Worker):
             return self._worker_repo.update_worker(worker)
         return False
+    
+    def get_by_id(self, id: int) -> Worker | str:
+        """
+        Retrieves a worker by their ID.
+        arguments:
+        - id: ID of the worker to retrieve.
+        returns:
+        - Returns the Worker object if found, otherwise returns an error message.
+        """
+        try:
+            worker = self._worker_repo.get_by_id(id)
+            if worker:
+                return worker
+            else:
+                raise Exception(f"Worker with the given ID : {id} was not found.")
+        except Exception as e:
+            return f"🛑 Error [{e}]"
 
