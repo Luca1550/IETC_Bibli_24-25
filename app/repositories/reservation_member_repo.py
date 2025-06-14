eb457af2e2eee94383cb8b92cdfc9793d3e331aa
coxi0 <corentin.germeau@gmail.com>
2025-06-14 21:48:58 +0200

    Dto repo

--- app/repositories/reservation_member_repo.py
+++ app/repositories/reservation_member_repo.py
@@ -1,12 +1,13 @@
 import pathlib
-from repositories.models import ReservationMember
+from repositories.models import ReservationMember, Member
+from repositories import MemberRepo
 from tools import JsonStorage
 
 class ReservationMemberRepo:
     PATH_RESERVATION_MEMBER_JSON=pathlib.Path(__file__).parent.parent.parent / "database" / "reservation_member.json"
     def __init__(self):
         self._reservation_member_json : list[ReservationMember] = JsonStorage.load_all(self.PATH_RESERVATION_MEMBER_JSON)
-
+        self.member_repo : MemberRepo = MemberRepo()
     def _save_all(self):
         JsonStorage.save_all(self.PATH_RESERVATION_MEMBER_JSON, self._reservation_member_json)
     
@@ -14,12 +15,23 @@ class ReservationMemberRepo:
         self._reservation_member_json.append(ReservationMember(id_member=id_member, id_reservation=id_reservation))
         self._save_all()
     def update_reservation_member(self,id_member:int,id_reservation:int):
-        for i,exist_res in enumerate(self._reservation_member_json):
-            if exist_res.id == id_member:
+        for exist_res in self._reservation_member_json:
+            if exist_res.id == id_reservation:
+                exist_res.id_member = id_member
                 self._save_all()
 
     def get_reservation_member(self,id_member:int,id_reservation:int):
-        pass
+        Listmemb : list[Member] = []
+        for truc in self._reservation_member_json:
+            if truc.id_reservation == id_reservation:
+                Listmemb.append(self.member_repo.get_member_by_id(truc.id_member))
+        return Listmemb
 
     def delete_reservation_member(self,id_member:int,id_reservation:int):
-        pass 
+        for things in self._reservation_member_json:
+            if things.id_reservation == id_reservation:
+                self._reservation_member_json.remove(things)
+                self._save_all()
+            return True
+        return False
+    
---
+++ app/services/models/reservation_dto.py
@@ -0,0 +1,6 @@
+from repositories.models import Member
+
+class ReservationDTO:
+    
+    def __init__(self):
+        pass
