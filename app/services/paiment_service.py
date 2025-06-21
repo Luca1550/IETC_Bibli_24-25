f6b3e185cfdabb819777c3405ab83f801abae6b8
coxi0 <corentin.germeau@gmail.com>
2025-06-21 22:13:17 +0200

    Add placeholder for fine calculation in PaiementService
    Imported BorrowService and added a stub method 'retunr' to prepare for implementing fine calculation based on borrow return dates. This sets up the structure for handling overdue payments.

--- app/services/paiment_service.py
+++ app/services/paiment_service.py
@@ -2,7 +2,7 @@ from repositories.models import Paiement, Member
 from repositories import paiement_repo, paiement_member_repo
 from .models import PaiementDTO
 from datetime import date, datetime
-from services import ExemplarService,LibraryService,MemberService
+from services import ExemplarService,LibraryService,MemberService,BorrowService
 from enums import PaiementType
 
 class PaiementService:
@@ -11,4 +11,14 @@ class PaiementService:
         self.library_service=LibraryService()
         self.member_service=MemberService()
         self.borrow_service=BorrowService()
+
 #crud + gestion des truc de library et relier au member
+#pour avoir la date de retunr du livre, voir dans member truc
+    #prendre date + retunr borrow date pour avooir le fine 
+
+
+
+#calculer le retard
+#Alin donne la date pour calculer le fine avec le borrow_retunr
+def retunr(self,date):
+    pass 
