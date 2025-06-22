f36bb689d367742be1ec67da9a3758715a048392
coxi0 <corentin.germeau@gmail.com>
2025-06-22 21:19:18 +0200

    Set main_panel background to transparent

--- app/ui/pages/library/library_page.py
+++ app/ui/pages/library/library_page.py
@@ -36,7 +36,7 @@ class LibraryPage(ctk.CTkFrame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)  
 
-        self.main_panel = ctk.CTkFrame(self)
+        self.main_panel = ctk.CTkFrame(self,fg_color="transparent")
         self.main_panel.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
         
 
